8e2a1d25a6891439b770c4868795e8c34b88e591
Kazuki Takahashi <kazukitash@aoikumo.jp>
2016-01-02 08:42:56 +0900

    load config file

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require "sinatra"
-require "slim"
+require "sinatra/config_file"
 require "sinatra/reloader" if development?
-require File.expand_path('../controller', __FILE__)
+require "slim"
+require "rexml/document"
+require File.expand_path("../controller", __FILE__)
+
+config_file File.expand_path("../config/config.yml", __FILE__)
+configure do
+  Dir.glob("config/**/*.xml").each do |c|
+    set File.basename(c, ".*").to_sym, REXML::Document.new(File.open(File.expand_path("../#{c}", __FILE__)))
+  end
+end
