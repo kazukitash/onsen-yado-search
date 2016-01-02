091feb34c4a22524d8776e31a9763d560a86d0be
Kazuki Takahashi <kazukitash@aoikumo.jp>
2016-01-02 23:18:56 +0900

    logger

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@ require "rexml/document"
 require "net/http"
 require "slim"
 require "uri"
+require "logger"
 require File.expand_path("../controller", __FILE__)
 Dir.glob("helpers/**/*.rb").each do |h|
   require File.expand_path("../#{h}", __FILE__)
+end
+
+class Logger
+  alias :write :<<
 end
 
 config_file File.expand_path("../config/config.yml", __FILE__)
@@ -15,4 +20,8 @@ configure do
   Dir.glob("config/**/*.xml").each do |c|
     set File.basename(c, ".*").to_sym, REXML::Document.new(File.open(File.expand_path("../#{c}", __FILE__)))
   end
+
+  logger = Logger.new("#{settings.root}/log/#{settings.environment}.log")
+  set :logger, logger
+  use Rack::CommonLogger, logger
 end
